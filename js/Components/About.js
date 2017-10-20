f6c67c19ad9ca0fc197d623852e830e439712d7c
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-10-20 16:40:20 -0700

    Can send email

--- js/Components/About.js
+++ js/Components/About.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   //
@@ -11,4 +17,26 @@ const about = (
   </View>
 );
 
-export default () => <View />;
+class T extends React.Component {
+  state = { handle: false };
+
+  click = async () => {
+    // encodeURIComponent
+    const can_click = await Linking.canOpenURL(
+      'mailto:someone@example.com?subject=This%20is%20the%20subject&cc=someone_else@example.com&body=This%20is%20the%20body'
+    );
+    console.warn(can_click);
+    this.setState({ handle: can_click });
+  };
+
+  render() {
+    return (
+      <View>
+        <Text onPress={this.click}>Hi</Text>
+        <Text>{this.state.handle ? 'CAN HANDLE' : 'I CAN NOT'}</Text>
+      </View>
+    );
+  }
+}
+
+export default T;
