189b4bdb0d8f8066b6e9fb68630bd4f3deaa95e8
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-10-20 14:42:05 -0700

    Use mobx, fill out the home screen

--- js/Components/PitchColumn.js
+++ js/Components/PitchColumn.js
@@ -6,22 +6,95 @@ import {
   TouchableOpacity,
   ScrollView,
 } from 'react-native';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Svg } from 'expo';
+
+import { padding_percent, Row } from '../elems';
 
 const styles = StyleSheet.create({
   centered: { alignItems: 'center' },
-  engage_banner: { fontSize: 32 },
+  banner: {
+    fontSize: 32,
+    textAlign: 'center',
+    padding: padding_percent,
+  },
 });
 
-const engage = [
-  <Text style={styles.engage_banner} key={1}>
-    Engage with Armenia
-  </Text>,
-  <Text key={2}>
-    Something more Something more Something more Something more Something more
-    Something more Something more Something more Something more Something more
-  </Text>,
-];
+const store = new function() {
+  extendObservable(this, {
+    action_modal: { show: false },
+  });
+}();
 
-export default () => {
-  return <View>{engage}</View>;
+const engage = (
+  <View style={{ backgroundColor: 'red' }}>
+    <Text style={styles.banner}>Engage with Armenia</Text>
+    <Text>
+      Something more Something more Something more Something more Something more
+      Something more Something more Something more Something more Something more
+    </Text>
+  </View>
+);
+
+const TakeAction = observer(() => {
+  return (
+    <View style={[styles.centered, { backgroundColor: 'orange' }]}>
+      <Text style={styles.banner}> Take Action</Text>
+      <Text>Send a request for online recognition to</Text>
+      <Row>
+        <Text>Select Category</Text>
+        <Text>End</Text>
+      </Row>
+    </View>
+  );
+});
+
+const DirectiveBlock = () => {
+  return (
+    <View>
+      <Text>Get Involved</Text>
+      <Text>Send Request</Text>
+      <Text>Report Success</Text>
+    </View>
+  );
 };
+
+const ResultCircle = ({ cx, cy }) => {
+  return (
+    <Svg.Circle
+      cx={cx}
+      cy={cy}
+      r={45}
+      strokeWidth={2.5}
+      stroke="#e74c3c"
+      fill="#f1c40f">
+      <Svg.Text fontSize="20" stroke="purple" fontWeight="bold" x={50} y={50}>
+        Hello
+      </Svg.Text>
+    </Svg.Circle>
+  );
+};
+
+const ResultsBlock = () => {
+  return (
+    <View>
+      <Text style={{ textAlign: 'center' }}>Results</Text>
+      <Svg style={{ backgroundColor: 'red' }} height={100} width={100}>
+        <ResultCircle cx={50} cy={50} />
+        <ResultCircle cx={70} cy={80} />
+      </Svg>
+    </View>
+  );
+};
+
+export default observer(() => {
+  return (
+    <View style={{ flex: 1 }}>
+      {engage}
+      <TakeAction />
+      <DirectiveBlock />
+      <ResultsBlock />
+    </View>
+  );
+});
--- js/elems.js
+++ js/elems.js
@@ -4,11 +4,15 @@ import { StyleSheet, View, Dimensions } from 'react-native';
 export const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
+    width: '100%',
+    backgroundColor: 'green',
   },
 });
 
 export const { height, width } = Dimensions.get('window');
 
-export const padding = Math.floor(width * 0.025);
+const padding_amount = 0.025;
+export const padding_percent = `${padding_amount}%`;
+export const padding = Math.floor(width * padding_amount);
 
 export const Row = ({ children }) => <View style={styles.row}>{children}</View>;
